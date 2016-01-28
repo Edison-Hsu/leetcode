f7b64c5da37e3eab34058e30330ed0ab1153eeec
Edison Hsu <xudongdonghome@gmail.com>
2016-01-28 21:50:39 +0800

    88. Merge Sorted Array

---
+++ 88. Merge Sorted Array/merge_sorted_array.rb
@@ -0,0 +1,21 @@
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+def merge(nums1, m, nums2, n)
+    tmp_nums = Array.new(nums1[0,m])
+    curr_tmp_nums, curr_nums1, curr_nums2 = 0, 0, 0
+    
+    (0...m+n).each do |n|
+        if  nums2[curr_nums2].nil? || (tmp_nums[curr_tmp_nums] && tmp_nums[curr_tmp_nums] <= nums2[curr_nums2])
+            number = tmp_nums[curr_tmp_nums]
+            curr_tmp_nums += 1
+        elsif tmp_nums[curr_tmp_nums].nil? || tmp_nums[curr_tmp_nums] > nums2[curr_nums2]
+            number = nums2[curr_nums2]
+            curr_nums2 += 1
+        end
+        nums1[curr_nums1] = number
+        curr_nums1 += 1
+    end
+end
